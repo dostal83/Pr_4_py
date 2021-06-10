b93da9b650d62c5ea12630138b5568aa4cdde50f
Nikoruso <62753545+dostal83@users.noreply.github.com>
2021-06-11 01:46:10 +0300

    Update pr 4.3.py

--- pr 4.3.py
+++ pr 4.3.py
@@ -1,32 +1,32 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import NearestNeighbors # Ввод библиотек
 
-#входные данные
+# Вводные данные
 A = np.array([[3.1, 2.3], [2.3, 4.2], [3.9, 3.5], [3.7, 6.4], [4.8, 1.9], 
              [8.3, 3.1], [5.2, 7.5], [4.8, 4.7], [3.5, 5.1], [4.4, 2.9]])
 
-#определяем ближайших соседей
+# Соседи
 k = 3
 
-#тестовые данные
+# Тест
 test_data = [3.3, 2.9]
 
-#визуализируем входные данные
+# Визуализиция ввода даных
 plt.figure()
 plt.title("input data")
 plt.scatter(A[:, 0], A[:, 1], marker = "o", s = 100, c = "black")
 
-#построим ближайшего соседа, обучим его 
+# Создание ближ. соседей с обучением
 knn_model = NearestNeighbors(n_neighbors = k, algorithm = "auto").fit(A)
 distances, indices = knn_model.kneighbors([test_data])
 
-#координаты K ближайших соседей
-print("\nk ближайших соседей: ")
+# Коорды ближ. Соседей
+print("\nk Neighboring neighbors: ")
 for rank, index in enumerate(indices[0][:k], start = 1):
     print(str(rank) + " is", A[index])
 
-#визуализируем K ближайших соседей
+#визуализируция соседей
 plt.title("K Nearest Neighbors")
 plt.scatter(A[:, 0], A[:, 1], marker="o", s=100, c="k")
 plt.scatter(A[indices][0][:][:, 0], A[indices][0][:][:, 1], marker = "o", s=250, facecolors = 'none', edgecolors='purple')
