7ff65f923c182f077df4c403ce2a1fce430ce78f
Nikoruso <62753545+dostal83@users.noreply.github.com>
2021-06-11 01:38:58 +0300

    Update pr 4.1.py

--- pr 4.1.py
+++ pr 4.1.py
@@ -3,22 +3,22 @@ import seaborn as sns;
 sns.set()
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn.datasets import make_blobs #набор данных
+from sklearn.datasets import make_blobs
 
-#набор данных
+# Данные
 X, y_true = make_blobs(n_samples = 500, centers = 4, cluster_std = 0.40, random_state = 0) 
 
-#визуализация набора данных
+# Вывод данных в терминал
 plt.scatter(X[:, 0], X[:, 1], s = 50)
 
-kmeans = KMeans(n_clusters = 4)#описываем модель kmeans в качестве алгоритма KMeans
+kmeans = KMeans(n_clusters = 4) 
 
-#обучение модели K-средних
+# Обучение мидл K
 kmeans.fit(X)
 y_kmeans = kmeans.predict(X)
 plt.scatter(X[:, 0], X[:, 1], c = y_kmeans, s = 50, cmap = 'viridis') #визуализация
 
-#центры
+# Мидл
 centers = kmeans.cluster_centers_
 plt.scatter(centers[:, 0], centers[:, 1], c = 'black', s = 200, alpha = 0.10)
 
